a5139dd0a1d989e0c6488235a3b0f445342f062b
MONO ᵈᵉᵛ <68543161+Mono-94@users.noreply.github.com>
2023-01-04 05:51:56 +0100

    boolean CustomSkill

--- client/menu.lua
+++ client/menu.lua
@@ -3,8 +3,9 @@ lib.locale()
 local function createSkillMenu()
     local gtaskills = {}
     local skills = {}
+    local options = {}
     for k, v in pairs(Config.Skills) do
-        if v.gtaAbility then 
+        if v.gtaAbility == true then 
             table.insert(gtaskills, {
                 title = v.SkillLang,
                 icon = v.icon,
@@ -39,33 +40,58 @@ local function createSkillMenu()
     
             })
         end
+
+        if Config.CustomSkill == true then 
+            lib.registerContext({
+                id = 'menu_skills_prin',
+                icon = 'signal-bars-good',
+                title = locale('skills'),
+                options = {
+                    {
+                        title = locale('gtaskills'),
+                        description = locale('gtaskillsdesc'),
+                        icon = 'person-circle-check',
+                        onSelect = function()
+                            lib.showContext('gtaskills_menu')
+                        end
+                    },
+                    {
+                        title = locale('customskills'),
+                        description = locale('customskillsdesc'),
+                        icon = 'person-digging',
+                        onSelect = function()
+                            lib.showContext('skills_menu')
+                        end
+                    },
+                },
+        
+        
+            })
+            lib.showContext('menu_skills_prin')
+        else
+            lib.registerContext({
+                id = 'menu_skills_prin',
+                icon = 'signal-bars-good',
+                title = locale('skills'),
+                options = {
+                    {
+                        title = locale('gtaskills'),
+                        description = locale('gtaskillsdesc'),
+                        icon = 'person-circle-check',
+                        onSelect = function()
+                            lib.showContext('gtaskills_menu')
+                        end
+                    },
+                },
+        
+        
+            })
+            lib.showContext('menu_skills_prin')
+        end
+        
     end
-    lib.registerContext({
-        id = 'menu_skills_prin',
-        icon = 'signal-bars-good',
-        title = locale('skills'),
-        options = {
-            {
-                title = locale('gtaskills'),
-                description = locale('gtaskillsdesc'),
-                icon = 'person-circle-check',
-                onSelect = function()
-                    lib.showContext('gtaskills_menu')
-                end
-            },
-            {
-                title = locale('customskills'),
-                description = locale('customskillsdesc'),
-                icon = 'person-digging',
-                onSelect = function()
-                    lib.showContext('skills_menu')
-                end
-            },
-        },
 
 
-    })
-    lib.showContext('menu_skills_prin')
 end
 
 RegisterCommand(Config.Skillmenu, function(args)
